27903c5a44aa127b580386d001220fa1b26599f0
Jorge Soares <jorgesoarescarvalhojsc@gmail.com>
2024-08-19 18:38:31 -0300

    Primeiro commit sprint 8

--- Challenger/support/base/baseTest.js
+++
@@ -1,6 +0,0 @@
-export { BaseChecks } from './baseChecks.js';
-export { BaseRest } from "../../services/baseRest.js";
-export { ENDPOINTS } from "./constants.js"
-export { testConfig } from "../config/environment.js"
-export { generateUser } from "../../data/dynamic/userFaker.js"
-export { generateProduct } from '../../data/dynamic/productFaker.js'
--- Challenger/support/base/constants.js
+++
@@ -1,6 +0,0 @@
-export const ENDPOINTS = {
-    LOGIN_ENDPOINT: '/login',
-    USER_ENDPOINT: '/usuarios',
-    PRODUCTS_ENDPOINT: '/produtos',
-    CARTS_ENDPOINT:'/carrinhos'
-}
